fbb38dcb8988265b0c0520edb297b9ed199b6cb5
SachinG99 <77461836+SachinG99@users.noreply.github.com>
2021-02-18 17:15:48 -0500

    Add files via upload

---
+++ diction.py
@@ -0,0 +1,25 @@
+print(" ")
+print(" ")
+print(" ")
+
+firstname = raw_input("Input First Person's Name: ")
+firstcar = raw_input("Input First Person's Car: ")
+secondname = raw_input("Input Second Person's Name: ")
+secondcar = raw_input("Input Second Person's Car: ")
+thirdname = raw_input("Input Third Person's Name: ")
+thirdcar = raw_input("Input Third Person's Car: ")
+
+#dict = {
+#    firstname : firstcar,
+#    secondname : secondcar,
+#    thirdname:thirdcar
+#}
+
+print(" ")
+print(" ")
+print("________________Vehicles Insured________________")
+print(firstname + " owns a " + firstcar + ".")
+print(secondname + " owns a " +  secondcar + ".")
+print(thirdname + " owns a " + thirdcar + ".")
+print("________________________________________________")
+print(" ")
